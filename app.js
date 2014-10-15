4f0e2a81e708025865afc86beea69d06f2e97d7b
warnerp18 <warnerp18@gmail.com>
2014-10-14 21:06:15 -0500

    update

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-```js
 _.templateSettings.interpolate = /{([\s\S]+?)}/g;
-```
 
-var someHTMLString = [
-     "<img src='{avatar_url}'>",
-].join("");
 
-function writeToDOM(data) {
+var someHtmlString = [
+	"<div 'class=sidebar'>",
+        "<img src='{avatar_url}'>",
+        "<h3>{name}</h3>",
+        "<ul>",
+            "<li> Blog: {html_url} </li>",
+            "<li> Location: {location} </li>",
+            "<li> Email: {email} </li>",
+        "</ul>",
+    "</div>",
+    "<div>",
+    	"<ul>",
+    		"<li><a href='html_url'> Contributions </a><li>",
+    		"<li><a href='{repo_url}'> Repositories </a><li>",
+    		"<li> </li>",
+    	"</ul>",
+    "</div>"
+    ].join("");
+
+function writeToDOM(data){
     document.body.innerHTML = _.template(someHtmlString, data);
 }
 
-$.getJSON("https://api.github.com/users/warnerp18").then(function(data){
-    console.log(data);
-})
+$.getJSON("https://api.github.com/users/warnerp18").then(writeToDOM);
